8132273dd4297ab4c6ef89ebe6f0cc8ed1af6f99
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-02-21 15:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-//your JS code here. If required.
 const textInput = document.getElementById("text");
 const delayInput = document.getElementById("delay");
 const button = document.getElementById("btn");
 const outputDiv = document.getElementById("output");
 
+// promise based timeout
 function wait(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-button.addEventListener("click", async function (e) {
-  e.preventDefault();   
+button.addEventListener("click", async (e) => {
+
+  e.preventDefault();
 
   const message = textInput.value;
-  const delay = Number(delayInput.value);
+  const delay = parseInt(delayInput.value, 10); 
 
+  
   outputDiv.textContent = "";
 
+
   await wait(delay);
+
 
   outputDiv.textContent = message;
 });
